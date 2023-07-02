ec779e421f1f09c2435c08f8eec7be8b724acbe0
Vishwa Mohan <kankvish@gmail.com>
2023-07-02 16:32:40 +0530

    adding user to db

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express  = require('express');
 const serverConfig =require('./configs/server.config');
 const mongoose = require('mongoose');
 const dbConfig = require('./configs/db.config');
+const userModel = require('./models/user.model');
 
 
 const app = express();
@@ -20,7 +21,35 @@ db.on("error", ()=>{
 
 db.once("open", ()=>{
     console.log("DB is connected");
+
+    init();
 })
+
+async function  init(){
+    
+    /**
+     * Check if the admin user is already present
+     */
+    let admin = await userModel.findOne({
+        userId : "admin"
+    })
+
+    if(admin){
+        console.log("Admin user already present");
+        return;
+    }
+
+    admin = await userModel.create( {
+        name : "Vishwa Mohan",
+        userId : "admin",
+        email : "kankvish@gmail.com",
+        userType : "ADMIN",
+        password : "Welcome1"
+    });
+    console.log(admin);
+
+    
+}
 
 
 
